3023d4b2af2a94f5be59cdce50f0ba88736d745c
Aaryan Patel <patel.aaryan.j@gmail.com>
2026-04-17 20:14:39 +1000

    course4

--- lib/posts.ts
+++ lib/posts.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 
-const PUBLISH_DIR = path.join(process.cwd(), 'content/publish')
+const PUBLISH_DIR = path.resolve(process.cwd(), 'content', 'publish')
 
 export type PostMeta = {
   slug: string
